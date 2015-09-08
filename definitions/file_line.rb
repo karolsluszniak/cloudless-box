fd21511df4c25e0a8d7f3b91ca8562ad55de786d
Karol Sluszniak <ksluszniak@gmail.com>
2015-09-08 21:53:58 +0200

    Extend file_line definition with a guard setting

--- definitions/file_line.rb
+++ definitions/file_line.rb
@@ -3,7 +3,8 @@ require 'shellwords'
 default_params = {
   file:    nil,
   content: nil,
-  filter:  nil
+  filter:  nil,
+  guard:   nil
 }
 
 define :file_line, default_params do
@@ -11,14 +12,17 @@ define :file_line, default_params do
   file = params[:file]
   content = Shellwords.escape(params[:content] || name)
   filter = params[:filter] && Shellwords.escape(params[:filter])
+  guard = params[:guard] && Shellwords.escape(params[:guard])
 
-  execute "file #{file} << #{name}" do
-    if filter
-      command "{ grep -Fv #{filter} #{file}; echo #{content}; } > #{file}"
+  execute "#{name} >> #{file}" do
+    if filter || guard
+      tmp_file = "#{file}.tmp-file-line"
+      command "{ grep -Fv #{filter || guard} #{file}; echo #{content}; } > #{tmp_file}" +
+              " && cat #{tmp_file} > #{file} && rm #{tmp_file}"
     else
       command "echo #{content} >> #{file}"
     end
 
-    not_if "grep -F #{filter || content} #{file}"
+    not_if "grep -F #{guard || content} #{file}"
   end
 end
